d6e4ac5b243ebaee58ad6f86187d06b46af7109b
SuperThunder <alexander.wolfe.10@googlemail.com>
2021-02-19 01:53:23 -0500

    #14 #15 Update the basic react-scripts test App.test.js for our app

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//We will fill in more tests here
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/FemtoGram/i);
