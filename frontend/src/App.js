0bb80993d9421cdc53ba4678f0c28f84bf2e5476
Tianyang-Jin11 <77936386+Tianyang-Jin11@users.noreply.github.com>
2021-02-20 20:11:26 -0500

    Update App.js
    update the APP file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,37 +8,39 @@ import HomepageComponent from "./components/homepage.component";
 import Profile from "./components/Profile/Profile.component";
 import Login from "./components/Login/login.component";
 import useToken from './useToken';
+import Gallery from './components/Gallery.component';
 
 
-function App() {
-    //const [token, setToken] = useState();
-    const { token, setToken } = useToken();
+function App () {
+  //const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
-    console.log("token in appjs", token);
+  console.log("token in appjs", token);
 
-    //look for a JWT token to check if user is already logged in
-    if(!token)
-    {
-        return(
-            <Router>
-                <Navbar />
-                <div className="container">
-                    <Login setToken={setToken} />
-                </div>
-            </Router>
-
-            );
-    }
-
-  return (
+  //look for a JWT token to check if user is already logged in
+  if (!token) {
+    return (
       <Router>
         <Navbar />
         <div className="container">
-          <br/>
-          <Route path="/" exact component={HomepageComponent} />
-          <Route path="/profile" exact component={Profile} />
+          <Login setToken={setToken} />
         </div>
       </Router>
+
+    );
+  }
+
+  return (
+    <Router>
+      <Navbar />
+      <div className="container">
+        <br />
+        <Route path="/" exact component={HomepageComponent} />
+        <Route path="/profile" exact  >
+          <Profile token={token} />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
