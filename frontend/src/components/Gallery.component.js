8157fbe4fada65502170699070d9e5f5fb4c8a1d
Tianyang-Jin11 <77936386+Tianyang-Jin11@users.noreply.github.com>
2021-02-23 14:35:18 -0500

    Update Gallery.component.js
    fix a bug which make the caption don't show on the picture

--- frontend/src/components/Gallery.component.js
+++ frontend/src/components/Gallery.component.js
@@ -9,11 +9,11 @@ const Gallery = ({posts}) => {
                 <input type="text" placeholder={"comments here"} style={{padding:'5px 10px', width:'90%'}}/>
                 <button style={{padding:'5px 10px', width:'10%', border:'none', borderRadius:'5px', backgroundColor:'#343a40', color:'white'}}>submit</button>
                 <div className='gallery-item-info'>
-                    {/*<ul>
+                    <ul>
                         <li className='gallery-item-likes'><span className='visually-hidden'></span>{post.caption}</li>
                         <br/>
                         <li className='gallery-item-comments'><span className='visually-hidden'>Comments:</span>0 <i className='fa fa-comment' aria-hidden/></li>
-                    </ul>*/}
+                    </ul>
                 </div>
             </div>
             })}
