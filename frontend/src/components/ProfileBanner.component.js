31a868545bab8afe82c2b4a47932c862033c81d2
Megan Walbaum <meganawalbaum@gmail.com>
2021-03-31 11:48:26 -0400

    #42 removed unused profile elements

--- frontend/src/components/ProfileBanner.component.js
+++ frontend/src/components/ProfileBanner.component.js
@@ -2,13 +2,8 @@ import React from 'react';
 const ProfileBanner = ({ addNewPost, followers, following, username, posts }) => {
     return (
         <div className='profile'>
-            <div className='profile-image'>
-                <img src='https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces' alt='User' />
-            </div>
             <div className="profile-user-settings">
                 <h1 className="profile-user-name">{username}</h1>
-                <button class='btn profile-edit-btn'>Edit Profile</button>
-                <button class='btn profile-settings-btn' aria-label="Profile Settings"><i className="fa fa-cog" aria-hidden></i></button>
             </div>
             <div className='profile-stats'>
                 <ul>
@@ -17,15 +12,8 @@ const ProfileBanner = ({ addNewPost, followers, following, username, posts }) =>
                     <li><span className='profile-stat-count'>{following.length}</span> following</li>
                 </ul>
             </div>
-            <div className='profile-bio'>
-                <p><span className='profile-real-name'>Header</span> Random Bio</p>
-            </div>
             <div className='profile-operations'>
-                <ul>
-                    <li><button className='btn profile-operations-btn'><i className='fa fa-user' aria-hidden></i></button></li>
-                    <li><button onClick={addNewPost} className='btn profile-operations-btn'><i className='fa fa-plus-circle' aria-hidden></i></button></li>
-                    <li><button className='btn profile-operations-btn'><i className='fa fa-history' aria-hidden></i></button></li>
-                </ul>
+                <li><button onClick={addNewPost} className='btn profile-operations-btn'><i className='fa fa-plus-circle' aria-hidden></i></button></li>
             </div>
         </div>
     );
