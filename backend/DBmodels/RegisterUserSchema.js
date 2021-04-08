584a87dc7bddd4533751093bce9e7ab1659f991a
Tianyang-Jin11 <1654108502abc@gmail.com>
2021-04-08 11:58:52 -0400

    Backend cleanup and name style
    check and fix files and variables name style problems. use static analysis to scan the code. delete unused codes

---
+++ backend/DBmodels/RegisterUserSchema.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+
+  username: { type: String, required: true, unique: true },
+  password: { type: String, minlength: 8, required: true },
+  email: { type: String, required: true },
+  followers: [],
+  following: [],
+  postsID: []
+})
+
+export default mongoose.model('User', userSchema)
